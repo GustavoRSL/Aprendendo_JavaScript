1a568893e0558cd8fcaf0449038e2db4cb64dcf8
GustavoRSL <d7gustavo@yahoo.com.br>
2021-12-22 15:09:40 -0300

    Teste

---
+++ Funções/basico_1.js
@@ -0,0 +1,20 @@
+// Function Declaration
+function bomDia() {
+  console.log('Bom dia!')
+}
+
+bomDia()
+
+// Function Expression
+const boaTarde = function () {
+  console.log('Boa Tarde!')
+}
+
+boaTarde()
+
+function soma(a, b) {
+  return a + b
+}
+
+const resultado = soma(3,4)
+console.log(resultado)
